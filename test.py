3c3e9632133820285fe9995e56e81d13e135956c
maheshdk4 <maheshdk46@gmail.com>
2025-09-25 17:11:11 +0530

    added bracket

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 print("Wassup Buddy!! v10")
 print("This message is from 25 SEP")
-print"This message is from 25 SEP - removing bracket in the front")
+print("This message is from 25 SEP - added bracket in the front")
